dcc12586f9a51fd6829549a50b373e0eaf8956c5
jacobshih <jacob_shih@hotmail.com>
2015-09-21 19:53:36 +0800

    What - copy the new torrents to the new folder. @Description/root cause - Why     it compares the filelist.txt with the last to check if there are new files     uploaded. if there are new files, it copies the new torrents to the folder     /YesDefy/new and prepends the new files list to file /YesDefy/new/newfiles.txt.     below is the format of /YesDefy/new/newfiles.txt:     ##############################################################################     # new files found at: 2015-09-21 17:09:49.009263     /YesDefy/00 Seed/ShooterDownloaderSetup_1-1-1-56.exe     /YesDefy/00 seed/20150920/18P2P_Reasonable.Doubt.2014.1080p.BluRay.x264.DTS-FGT.torrent     /YesDefy/00 seed/20150920/P.K 2014 HINDI NonRetail 1080p Bluray  x264 DTS - TeamTelly.1.torrent
    @Resolution - How
        as described above.
    
    @Reviewer
    ...

--- dbfind/dbfind.py
+++ dbfind/dbfind.py
@@ -5,12 +5,23 @@ import os
 import sys
 import json
 import datetime
+import difflib
 from dropbox import client, rest, session
 
-FILE_LIST = 'filelist.txt'
 FOLDER_YESDEFY = '/YesDefy'
+FOLDER_NEW = FOLDER_YESDEFY + '/new'
+FILE_FILELIST = 'filelist.txt'
+FILE_FILELIST_LAST = 'filelist_last.txt'
+FILE_NEWFILES = 'newfiles.txt'
+
+HEADER_FILELIST_UPDATED = '# last updated at:'
+HEADER_TO_FILE = '+++'
+HEADER_NEWFILES_UPDATED = '# new files found at:'
+HEADER_SEPARATOR = '##############################################################################'
+EXT_TORRENT = '.torrent'
 
 class MyDropbox():
+    new_files = []
     def __init__(self, token_file):
         self.api_client = None
         try:
@@ -30,13 +41,14 @@ class MyDropbox():
             else:
                 print "Malformed access token in %r." % (self.token_file,)
         except IOError:
-            pass # don't worry if it's not there
+            pass
+        pass
 
-    def do_find(self, folder="/"):
+    def db_find(self, folder="/"):
         if self.api_client is None:
             exit()
         client = self.api_client
-        files = {}
+        allfiles = {}
         cursor=None
         has_more = True
         while has_more:
@@ -46,41 +58,116 @@ class MyDropbox():
 
         for lowercase_path, metadata in result['entries']:
             if metadata is not None:
-                files[lowercase_path] = metadata
+                allfiles[lowercase_path] = metadata
             else:
-                # no metadata indicates a deletion
-                # remove if present
-                files.pop(lowercase_path, None)
+                # remove it if there is no metadata that indicates a deletion.
+                allfiles.pop(lowercase_path, None)
 
-        # in case this was a directory, delete everything under it
-        for other in files.keys():
-            if other.startswith(lowercase_path + '/'):
-                del files[other]
+        files = []
+        for f in allfiles.keys():
+            if not allfiles[f]['is_dir']:
+                filename = allfiles[f]['path']
+                if filename.startswith(FOLDER_YESDEFY):
+                    files.append(filename)
+        files.sort()
+        return files
 
-        paths = []
-        for path in files.keys():
-            if not files[path]['is_dir']:
-                paths.append(files[path]['path'])
-        paths.sort()
-        return paths
+    def db_put(self, from_path, to_path):
+        with open(os.path.expanduser(from_path), 'rb') as from_file:
+            encoding = locale.getdefaultlocale()[1] or 'ascii'
+            full_path = to_path.decode(encoding)
+            self.api_client.put_file(full_path, from_file, True)
+        pass
 
-    def do_write_file(self, filename, data):
-        f = open(filename, 'w')
-        f.write('# last updated at: ' + str(datetime.datetime.now()) + '\r\n')
-        f.write('\r\n')
-        for d in data:
-            f.write(d.encode('utf8')+'\r\n')
-        f.close()
+    def db_get(self, from_path, to_path):
+        with open(os.path.expanduser(to_path), "wb") as to_file:
+            try:
+                f, metadata = self.api_client.get_file_and_metadata(from_path)
+                to_file.write(f.read())
+            except rest.ErrorResponse as e:
+                print '(%d)[%s] %s (%s)' % (e.status, e.reason, e.error_msg, from_path)
+                to_file.write('')
+        pass
 
-    def do_put(self, from_path, to_path):
-        from_file = open(os.path.expanduser(from_path), 'rb')
-        encoding = locale.getdefaultlocale()[1] or 'ascii'
-        full_path = to_path.decode(encoding)
-        self.api_client.put_file(full_path, from_file, True)
+    def db_copy(self, from_path, to_path):
+        self.api_client.file_copy(from_path, to_path)
+        pass
+
+    def check_new_files(self , from_file, to_file):
+        self.new_files = []
+        with open(FILE_FILELIST, 'r') as filelist:
+            with open(FILE_FILELIST_LAST, 'r') as filelist_last:
+                diff = difflib.unified_diff(
+                    filelist_last.readlines(),
+                    filelist.readlines(),
+                    fromfile='filelist_last',
+                    tofile='filelist',
+                )
+                for line in diff:
+                     line = line.strip('\n\r')
+                     if line.startswith('+'):
+                         if line.startswith(HEADER_TO_FILE):
+                             continue
+                         elif line[1:].startswith(HEADER_FILELIST_UPDATED):
+                             continue
+                         elif not line[1:].startswith(FOLDER_YESDEFY):
+                             continue
+                         elif line[1:].startswith(FOLDER_NEW):
+                             continue
+                         else:
+                            self.new_files.append(line[1:])
+                     pass
+                pass
+            pass
+        pass
+        return self.new_files
+
+    def write_file(self, filename, data):
+        with open(filename, 'w') as f:
+            for d in data:
+                f.write(d.encode('utf8')+'\r\n')
+        pass
+
+    def write_filelist(self, filename, data):
+        filelist_log = []
+        filelist_log.append(('%s %s' % (HEADER_FILELIST_UPDATED, str(datetime.datetime.now()))))
+        filelist_log.append('')
+        filelist_log.extend(data)
+        self.write_file(filename, filelist_log)
+        pass
+
+    def copy_new_torrents_to_folder(self, new_files, to_folder):
+        new_files_logs = []
+        new_files_logs.append(HEADER_SEPARATOR)
+        new_files_logs.append('%s %s' % (HEADER_NEWFILES_UPDATED, str(datetime.datetime.now())))
+        for new_file in new_files:
+            new_files_logs.append(new_file)
+            if new_file.endswith(EXT_TORRENT):
+                head, tail = os.path.split(new_file)
+                filename = tail
+                try:
+                    self.db_copy(new_file, to_folder + '/' + filename)
+                except rest.ErrorResponse as e:
+                    new_files_logs.append('(%d)[%s] %s' % (e.status, e.reason, e.error_msg))
+                pass
+            pass
+
+        last_content = None
+        with open(FILE_NEWFILES, 'r+') as f:
+            last_content = f.read()
+
+        new_files_logs.append('')
+        new_files_logs.append(last_content)
+        self.write_file(FILE_NEWFILES, new_files_logs)
+        pass
+
+    def is_new_files(self):
+        return True if len(self.new_files) is not 0 else False
 
 def show_usage():
     print "usage:"
     print "  python " + os.path.basename(__file__) + " {tokens.json}"
+    pass
 
 def main():
     if len(sys.argv) < 2:
@@ -88,9 +175,19 @@ def main():
         exit()
     tokens = sys.argv[1]
     myDropbox = MyDropbox(tokens)
-    paths = myDropbox.do_find()
-    myDropbox.do_write_file(FILE_LIST, paths)
-    myDropbox.do_put(FILE_LIST, FOLDER_YESDEFY + '/' + FILE_LIST)
+    myDropbox.db_get(FOLDER_YESDEFY + '/' + FILE_FILELIST, FILE_FILELIST_LAST)
+    myDropbox.db_get(FOLDER_NEW + '/' + FILE_NEWFILES, FILE_NEWFILES)
+    files = myDropbox.db_find()
+    myDropbox.write_filelist(FILE_FILELIST, files)
+
+    new_files = myDropbox.check_new_files(FILE_FILELIST_LAST, FILE_FILELIST)
+    if myDropbox.is_new_files():
+        myDropbox.copy_new_torrents_to_folder(new_files, FOLDER_NEW)
+        myDropbox.db_put(FILE_FILELIST, FOLDER_YESDEFY + '/' + FILE_FILELIST)
+        myDropbox.db_put(FILE_NEWFILES, FOLDER_NEW + '/' + FILE_NEWFILES)
+        pass
+    pass
 
 if __name__ == '__main__':
     main()
+    pass
